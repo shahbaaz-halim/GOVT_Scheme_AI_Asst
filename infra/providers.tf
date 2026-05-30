9439d0d448ee3c6e0585a0bdca309c0eb5d1bf90
shahbaaz-halim <halimshahbaaz@gmal.com>
2026-05-30 18:45:06 +0530

    s3 and iam roles for lambda

---
+++ infra/providers.tf
@@ -0,0 +1,24 @@
+# infra/provider.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Always pin your provider version to avoid breaking changes!
+    }
+  }
+  
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  # Default tags are a lifesaver. Every resource created will automatically get these.
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = "Production"
+      ManagedBy   = "Terraform"
+    }
+  }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,13 @@
+
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "ap-south-2" 
+}
+
+variable "project_name" {
+  description = "Base name for project resources"
+  type        = string
+  default     = "govt-scheme-ai-asst"
+}
